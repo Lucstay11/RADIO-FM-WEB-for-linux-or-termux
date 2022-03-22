fce868ba674198c941aba8689f1fb690297d685a
lucstay11 <89875785+Lucstay11@users.noreply.github.com>
2022-03-22 18:28:24 +0100

    Update radio.sh

--- radio.sh
+++ radio.sh
@@ -5,6 +5,11 @@ rouge='\e[1;31m'
 bleu='\e[1;34m'
 blanc='\e[1;35m'
 jaune='\e[1;33m'
+
+function info(){
+echo -e $rouge "[$blanc q $rouge] $blanc Quitter"   "$rouge[$blanc m $rouge] $blanc Muter"
+}
+
 function debut(){
 echo -e $bleu " ██████╗  █████╗ ██████╗ ██╗ ██████╗  "
 echo -e $bleu " ██╔══██╗██╔══██╗██╔══██╗██║██╔═══██╗ "
@@ -52,59 +57,88 @@ sleep 2
 exit 
 else
 case $radio in
-            1)mpv http://radios.rtbf.be/classic21-128.mp3
+            0)exit
             ;;
-            2)mpv http://radios.rtbf.be/jam-128.mp3
+            1)info
+              mpv http://radios.rtbf.be/classic21-128.mp3
             ;;
-            3)mpv http://radios.rtbf.be/laprem1ere-128.mp3
+            2)info
+              mpv http://radios.rtbf.be/jam-128.mp3
             ;;
-            4)mpv https://radios.rtbf.be/musiq3-128.mp3
+            3)info
+              mpv http://radios.rtbf.be/laprem1ere-128.mp3
             ;;
-            5)mpv https://radios.rtbf.be/pure-128.mp3
+            4)info
+              mpv https://radios.rtbf.be/musiq3-128.mp3
             ;;
-            6)mpv https://radios.rtbf.be/vivabxl-128.mp3 
+            5)info
+              mpv https://radios.rtbf.be/pure-128.mp3
             ;;
-            7)mpv http://broadcast.infomaniak.ch/arabelprodcastfm.mp3
+            6)info
+              mpv https://radios.rtbf.be/vivabxl-128.mp3 
             ;;
-            8)mpv http://belrtl.ice.infomaniak.ch/belrtl-mp3-128.mp3
+            7)info
+              mpv http://broadcast.infomaniak.ch/arabelprodcastfm.mp3
             ;;
-            9)mpv http://rmc.scdn.arkena.com/rmc.mp3
+            8)info
+              mpv http://belrtl.ice.infomaniak.ch/belrtl-mp3-128.mp3
             ;;
-            10)mpv http://dhradio.ice.infomaniak.ch/dhradio-192.mp3
+            9)info
+               mpv http://rmc.scdn.arkena.com/rmc.mp3
             ;;
-            11)mpv http://live.funradio.be/funradiobe-high.mp3
+            10)info
+               mpv http://dhradio.ice.infomaniak.ch/dhradio-192.mp3
             ;;
-            12)mpv http://cdn.nrjaudio.fm/audio1/fr/30601/aac_64.mp3?origine=fluxradios
+            11)info
+               mpv http://live.funradio.be/funradiobe-high.mp3
             ;;
-            13)mpv http://streamingp.shoutcast.com/NostalgieSoulparty-aac
+            12)info
+               mpv http://cdn.nrjaudio.fm/audio1/fr/30601/aac_64.mp3?origine=fluxradios
             ;;
-            14)mpv http://streamingp.shoutcast.com/NRJ
+            13)info
+               mpv http://streamingp.shoutcast.com/NostalgieSoulparty-aac
             ;;
-            15)mpv https://radios.rtbf.be/pure-128.mp3 
+            14)info
+               mpv http://streamingp.shoutcast.com/NRJ
             ;;
-            16)mpv https://radios.rtbf.be/wr-pure2-128.mp3
+            15)info
+               mpv https://radios.rtbf.be/pure-128.mp3
             ;;
-            17)mpv http://91.121.38.100:8310/;stream/1
+            16)info
+               mpv https://radios.rtbf.be/wr-pure2-128.mp3
             ;;
-            18)mpv https://start-sud.ice.infomaniak.ch/start-sud-high.mp3
+            17)info
+               mpv http://91.121.38.100:8310/;stream/1
             ;;
-            19)mpv  http://rmc.bfmtv.com/rmcinfo-mp3
+            18)info
+               mpv https://start-sud.ice.infomaniak.ch/start-sud-high.mp3
             ;;
-            20)mpv  http://stream.europe1.fr/europe1.mp3
+            19)info
+               mpv  http://rmc.bfmtv.com/rmcinfo-mp3
             ;;
-            21)mpv  http://icecast.rtl2.fr/rtl2-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg
+            20)info
+               mpv http://stream.europe1.fr/europe1.mp3
             ;;
-            22)mpv https://icecast.radiofrance.fr/franceinter-midfi.mp3
+            21)info
+               mpv http://icecast.rtl2.fr/rtl2-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg
             ;;
-            23)mpv http://scdn.nrjaudio.fm/adwz2/fr/30401/mp3_128.mp3?origine=fluxradios
+            22)info
+               mpv https://icecast.radiofrance.fr/franceinter-midfi.mp3
             ;;
-            24)mpv http://icecast.rtl2.fr/rtl2-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg
+            23)info
+               mpv http://scdn.nrjaudio.fm/adwz2/fr/30401/mp3_128.mp3?origine=fluxradios
             ;;
-            25)mpv https://radios.rtbf.be/wr-c21-70-128.mp3 
+            24)info
+               mpv http://icecast.rtl2.fr/rtl2-1-44-128?listen=webCwsBCggNCQgLDQUGBAcGBg
             ;;
-            26)mpv http://radiocontact.ice.infomaniak.ch/radiocontact-mp3-128.mp3
+            25)info
+               mpv https://radios.rtbf.be/wr-c21-70-128.mp3
             ;;
-            27)mpv https://bxfmradio.ice.infomaniak.ch/bxfmradio-192.mp3
+            26)info
+               mpv http://radiocontact.ice.infomaniak.ch/radiocontact-mp3-128.mp3
+            ;;
+            27)info
+               mpv https://bxfmradio.ice.infomaniak.ch/bxfmradio-192.mp3
             ;;
             *)echo -e $bleu_clair "Le numero $radio n'est pas encore dans la liste!"
               sleep 2
